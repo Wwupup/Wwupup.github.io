8ab1c9638b3091cd421c09f5da0a7ebfb2927b48
Wwupup <wuweimyself@qq.com>
2022-12-22 11:35:33 +0800

    init

---
+++ docs/collect.md
@@ -0,0 +1,100 @@
+# 宝藏收藏夹
+这是收集各种博客、网址、学习资料、文章和课程的地方。
+
+## 教程类
+
+[SIGGRAPH历年仿真论文](http://www.physicsbasedanimation.com/)
+
+[连续介质力学入门](https://www.continuummechanics.org/)
+
+[极简的ctest学习博客](https://matgomes.com/cmake-ctest-to-add-cpp-tests/)
+
+[learn-opengl](https://learnopengl-cn.github.io/)
+
+[zeno文档](http://doc.zenustech.com/)
+
+[1997年SIGGRAPH刚体模拟Course:Witkin&Baraff](http://www.cs.cmu.edu/~baraff/sigcourse/index.html)
+
+[刚体模拟的一个博客](https://www.toptal.com/game/video-game-physics-part-i-an-introduction-to-rigid-body-dynamics)
+
+[python的四元数库](https://quaternion.readthedocs.io/en/latest/)
+
+[谷歌C++风格指南](https://zh-google-styleguide.readthedocs.io/en/latest/contents/)
+
+[Godot(开源游戏引擎)入门](https://docs.godotengine.org/en/stable/getting_started/introduction/first_look_at_the_editor.html)
+
+[python3菜谱](https://python3-cookbook.readthedocs.io/zh_CN/latest/index.html)
+
+[pybind11文档](https://pybind11.readthedocs.io/en/stable/index.html)
+
+[effective C++(博客学习笔记)](https://harttle.land/2015/09/03/effective-cpp-36.html)
+
+[关于符号距离场的一个博客](https://airguanz.github.io//2021/06/28/sdf-gen.html)
+
+[openvdb菜谱(官方)](https://www.openvdb.org/documentation/doxygen/codeExamples.html#sHelloWorld)
+
+[OpenMP文档](https://cliutils.gitlab.io/modern-cmake/chapters/packages/OpenMP.html)
+
+[重构你的cpp程序以增强可测性](https://github.com/platisd/refactoring-for-testability-cpp)
+
+[游戏流体力学及Unity代码](https://github.com/clatterrr/FluidSimulationTutorialsUnity)
+
+[利用cmake配置好opengl环境](https://shot511.github.io/2018-05-29-how-to-setup-opengl-project-with-cmake/)
+
+[Houdini Vex 插件](http://cgtoolbox.com/houdini-expression-editor/)
+
+## 课程类
+
+[games101课程大纲和ppt](https://sites.cs.ucsb.edu/~lingqi/teaching/games101.html)
+
+[CS839: 威斯康辛大学Sifakis的图形学仿真课程](https://pages.cs.wisc.edu/~sifakis/courses/cs839-f19/)
+
+[Bender的基于物理动画入门课程资源](https://interactivecomputergraphics.github.io/physics-simulation/)
+
+[SIGGRAPH2019基于物理动画入门课](https://education.siggraph.org/cgsource/content/introduction-physics-based-animation)
+
+[Dynamic Deformables Course | Pixar Animation Siggraph 2020 Talk](https://www.youtube.com/watch?app=desktop&v=ZCQbwXqbtS8)
+
+[CPSC314: UBC的图形学入门课程](https://www.students.cs.ubc.ca/~cs-314/Vjan2008/schedule.html#week1)
+
+[CS344: NVIDIA UDACITY的并行入门课](https://developer.nvidia.com/udacity-cs344-intro-parallel-programming)
+
+[汪洋的OpenFOAM课程ppt](https://github.com/wo315/2206CFD_OpenFOAM)
+
+## 书籍类
+
+[cmake菜谱(系统学习cmake最好的书)](https://www.bookstack.cn/read/CMake-Cookbook/content-preface-preface-chinese.md)
+
+[modern cmake中文版](https://modern-cmake-cn.github.io/Modern-CMake-zh_CN/)
+
+[Professional cmake](https://crascit.com/professional-cmake/)
+
+[游戏编程设计模式](https://gameprogrammingpatterns.com/)
+
+[用python自动化无聊的工作](https://automatetheboringstuff.com/)
+
+[Rust程序设计语言](https://kaisery.github.io/trpl-zh-cn/title-page.html)
+
+[现代C++教程:高速上手(快速了解modern c++)](https://changkun.de/modern-cpp/zh-cn/00-preface/)
+
+[书籍:游戏编程模式](https://gpp.tkchu.me/)
+
+[可交互的Shader学习:The Book of Shader](https://thebookofshaders.com/?lan=ch)
+
+[GPU Gems(图形学大部头经典)](https://developer.nvidia.com/gpugems/gpugems/contributors)
+
+## 资源类
+
+[便宜的CG资源:cgtrader](https://www.cgtrader.com/)
+
+[CG资源turbosquid](https://www.turbosquid.com/)
+
+[CG资源sketchfab](https://sketchfab.com/)
+
+[免费HDR和纹理:polyhaven](https://polyhaven.com/)
+
+[c语言稀疏线性求解器库](https://www.tau.ac.il/~stoledo/taucs/)
+
+[星球渲染纹理资源](https://www.solarsystemscope.com/textures/)
+
+[生成好看的配色:coolors](https://coolors.co/)
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,18 +7,19 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Wu Wei',
-  tagline: 'Sustech student',
+  tagline: '',
   url: 'https://Wwupup.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/site_logo.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Wwupup', // Usually your GitHub org/user name.
   projectName: 'Wwupup.github.io', // Usually your repo name.
   deploymentBranch: 'deployment',
+  trailingSlash: false,
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
@@ -33,18 +34,21 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/Wwupup/Wwupup.github.io/tree/master',
+          // editUrl:
+          //   'https://github.com/Wwupup/Wwupup.github.io/tree/master',
         },
         blog: {
           showReadingTime: true,
+          blogSidebarTitle: '全部文章',
+          blogSidebarCount: 'ALL',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/Wwupup/Wwupup.github.io/tree/master/blog',
+          // editUrl:
+          //   'https://github.com/Wwupup/Wwupup.github.io/tree/master/blog',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -57,11 +61,11 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
+        title: 'Home',
+        // logo: {
+        //   alt: 'My Site Logo',
+        //   src: 'img/logo.svg',
+        // },
         items: [
           {
             type: 'doc',
@@ -80,39 +84,39 @@ const config = {
       footer: {
         style: 'dark',
         links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
+          // {
+          //   title: 'Docs',
+          //   items: [
+          //     {
+          //       label: 'Tutorial',
+          //       to: '/docs/intro',
+          //     },
+          //   ],
+          // },
+          // {
+          //   title: 'Community',
+          //   items: [
+          //     {
+          //       label: 'Stack Overflow',
+          //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+          //     },
+          //     {
+          //       label: 'Discord',
+          //       href: 'https://discordapp.com/invite/docusaurus',
+          //     },
+          //     {
+          //       label: 'Twitter',
+          //       href: 'https://twitter.com/docusaurus',
+          //     },
+          //   ],
+          // },
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
+              // {
+              //   label: 'Blog',
+              //   to: '/blog',
+              // },
               {
                 label: 'GitHub',
                 href: 'https://github.com/Wwupup',
@@ -120,7 +124,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Wwupup, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
